a9e8a07cf4b7b953625702c7b07b26a127fbe13c
Isabella Namutenda <namtendaisabella@gmail.com>
2022-04-10 20:24:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change it
+// If you want your app to work offline and load faster, you can change it.
 // unregister() to register()
   
